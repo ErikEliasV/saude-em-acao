26e69645e942fcc0818fcadd6847228863ecfc50
Erik <erik-lindolias@hotmail.com>
2025-02-20 20:50:25 -0300

    adicionando mais funcoes tela perfil

---
+++ static/js/scripts/contagem.js
@@ -0,0 +1,20 @@
+function atualizarContagem() {
+
+    let checkboxes = document.querySelectorAll('.checkbox');
+    let checkedCount = 0;
+
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            checkedCount++;
+        }
+    });
+
+    document.getElementById('checkedCount').textContent = checkedCount;
+}
+
+let checkboxes = document.querySelectorAll('.checkbox');
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', atualizarContagem);
+});
+
+atualizarContagem();
