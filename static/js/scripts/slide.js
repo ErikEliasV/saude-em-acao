a185fc1bfcfe7f12dbaa7c2ade20be1c35e1ed48
Erik <erik-lindolias@hotmail.com>
2025-02-25 21:09:38 -0300

    Finalização total - com 0 erros

--- static/js/scripts/slide.js
+++ static/js/scripts/slide.js
@@ -1,35 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var swiper = new Swiper(".swiper", {
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },  
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        keyboard: {
-            enabled: true,
-        },
-        mousewheel: {
-            thresholdDelta: 70,
-        },
-        spaceBetween: 30,
-        loop: true,
-        loopAdditionalSlides: 1,
-        breakpoints: {
-            640: {
-                slidesPerView: 2,
+    Promise.all(Array.from(document.images).map(img => {
+        if (img.complete) return Promise.resolve();
+        return new Promise(resolve => {
+            img.onload = img.onerror = resolve;
+        });
+    })).then(() => {
+        var swiper = new Swiper(".swiper", {
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },  
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            keyboard: {
+                enabled: true,
             },
-            1024: {
-                slidesPerView: 3,
+            mousewheel: {
+                thresholdDelta: 70,
             },
-        },
-        coverflowEffect: {
-            rotate: 0,
-            stretch: 0,
-            depth: 0, 
-            modifier: 1,
-            slideShadows: false 
-        }
+            spaceBetween: 10,
+            loop: true,
+            loopAdditionalSlides: 1,
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+            },
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 0,
+                depth: 0, 
+                modifier: 1,
+                slideShadows: false 
+            }
+        });
     });
 });
